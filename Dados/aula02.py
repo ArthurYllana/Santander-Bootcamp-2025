4ad7324e46dde4c54df581b5af8e21d92357f9a0
ArthurYllana <arthur.yllana@gmail.com>
2025-07-01 22:51:18 -0300

    Adição de conteudo novo

---
+++ Dados/aula02.py
@@ -0,0 +1,39 @@
+print (11 + 10)
+print (1.8 + 11)
+print("================")
+print(True)
+print(False)
+print("================")
+
+print("Modo Interativo")
+print("Váriaveis e Constantes")
+
+idade = 23 #int 
+nome= "Guilherme" #string
+print(f"Meu nome é {nome} e eu tenho {idade} ano(s) de idade")
+
+idade = 28 #int 
+nome= "Amanda" #string
+print(f"Meu nome é {nome} e eu tenho {idade} ano(s) de idade")
+
+print("================")
+
+STATES = ["SP", "RJ", "SC", "RS"]
+
+print("================")
+
+print("Conversão de dados")
+preco = 10
+print(preco /2)
+
+texto = f"idade {idade} preço {preco}"
+print (texto)
+
+preco2 = "10.00"
+print(float(preco2))
+
+print("================")
+print("Funções de Entrada e Saida")
+
+nome1 = input("Infome um nome: ")
+print = (nome1)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Santander-Bootcamp-2025
 Curso de 58 horas Santander
+
+As primeiras aulas são para relembrar conceitos básicos 
