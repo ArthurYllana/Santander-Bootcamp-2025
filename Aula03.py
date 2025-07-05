495c2f12e8dda0c6e55168efa338b9fcf9655003
ArthurYllana <arthur.yllana@gmail.com>
2025-07-04 21:54:01 -0300

    Adição de conteudo novo

---
+++ Aula03.py
@@ -0,0 +1,13 @@
+print("Operadores Aritméticos!")
+
+saldo = 200
+saque = 200
+saldo += 200
+print(saque == saldo)
+print(saque < saldo)
+print(saque > saldo)
+print(saque != saldo)
+
+print(10 + 3 + (2*5) - (2/2))
+
+pri
---
+++ Aula04.py
@@ -0,0 +1,29 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado!")
+
+
+def depositar(valor):
+    saldo = 500
+    salor += valor
+
+
+sacar(100)
+
+#Estrutura condicionais 
+
+Maior_idade = 18
+Idade_especial = 20
+
+idade = int(input("Qual sua idade: "))
+
+if idade >= Maior_idade:
+    print("Maior de idade, pode tirar CNH")
+elif idade == Idade_especial:
+    print("Você já tem CNH.... EU SEI QUEM VOCÊ É")
+else:
+    print("Menor de idade, não pode tirar CNH, volte quando tiver 18")
+
+
